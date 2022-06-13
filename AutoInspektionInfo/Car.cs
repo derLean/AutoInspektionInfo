8b14e0d1c265077fb459f8ac173e11e62d126693
derLean <107419578+derLean@users.noreply.github.com>
2022-06-13 19:56:24 +0200

    Add files via upload

---
+++ AutoInspektionInfo/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoInspection2
+{
+    class Car
+    {
+        public string carName = "Audi";
+        public int baujahr = 1995;
+        public int lastInspectionYear = 2010;
+
+
+        //Konstruktor
+        public Car(string newName, int newBaujahr)
+        {
+            carName = newName;
+            baujahr = newBaujahr;
+        }
+
+        public void Inspect()
+        {
+            lastInspectionYear = 2022;
+        }
+    }
+}
---
+++ AutoInspektionInfo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AutoInspection2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car[] cars = new Car[7];
+            cars[0] = new Car("Mercedes G", 2008);
+            cars[1] = new Car("BMW 3", 2001);
+            cars[2] = new Car("Audi A6", 2005);
+            cars[3] = new Car("Audi A8", 2012);
+            cars[4] = new Car("Hyundai", 2003);
+            cars[5] = new Car("Toyota", 1995);
+            cars[6] = new Car("Porsche 911 GT3", 2021);
+
+            Console.WriteLine("Bitte geben sie einen Suchbegriff ein:");
+
+            string searchTerm = Console.ReadLine();
+
+            Console.WriteLine("Suchbegriff ist: " + searchTerm);
+
+            Console.WriteLine("Folgende Autos wurden gefunden:");
+
+            foreach (Car car in cars)
+            {
+                if (car.carName.Contains(searchTerm))
+                {
+                    Console.WriteLine(car.carName + " Baujahr: " + car.baujahr + " Letzte Inspection: " + car.lastInspectionYear);
+                    car.Inspect();
+                    Console.WriteLine("Neues Inspektionsjahr: " + car.lastInspectionYear);
+                }
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+            Main();
+
+        }
+    }
+}

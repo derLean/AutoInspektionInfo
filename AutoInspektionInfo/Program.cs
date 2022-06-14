041dfafaeffe50feebaf8e73404471ab6d7b3aa9
derLean <107419578+derLean@users.noreply.github.com>
2022-06-14 14:23:26 +0200

    Update Program.cs

--- AutoInspektionInfo/Program.cs
+++ AutoInspektionInfo/Program.cs
@@ -7,14 +7,14 @@ namespace AutoInspection2
         static void Main()
         {
             Car[] cars = new Car[8];
-            cars[0] = new Car("Mercedes G", 2008);
-            cars[1] = new Car("BMW 3", 2001);
-            cars[2] = new Car("Audi A6", 2005);
-            cars[3] = new Car("Audi A8", 2012);
-            cars[4] = new Car("Hyundai", 2003);
-            cars[5] = new Car("Toyota", 1995);
-            cars[6] = new Car("Porsche 911 GT3", 2021);
-            cars[7] = new Car("VW Golf R32", 2002);
+            cars[0] = new Car("Mercedes G", 2008, 2014);
+            cars[1] = new Car("BMW 3", 2001, 2010);
+            cars[2] = new Car("Audi A6", 2005, 2018);
+            cars[3] = new Car("Audi A8", 2012, 2018);
+            cars[4] = new Car("Hyundai", 2003, 2006);
+            cars[5] = new Car("Toyota", 1995, 2011);
+            cars[6] = new Car("Porsche 911 GT3", 2021, 2021);
+            cars[7] = new Car("VW Golf R32", 2002, 2016);
 
             Console.WriteLine("Bitte geben sie einen Suchbegriff ein:");
 
